d25ffbb65cbc96b8a304d98acec7f4a1fa981548
nagasudhirpulla <nagasudhirpulla@gmail.com>
2017-08-22 16:20:22 +0530

    minor UI changes

--- app/src/main/java/com/example/nagasudhir/debtonator/HomeActivity.java
+++ app/src/main/java/com/example/nagasudhir/debtonator/HomeActivity.java
@@ -228,7 +228,7 @@ public class HomeActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            loadTranSetsBtn(null);
         }
     }
 
